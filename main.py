2f94021ec4c90dc6be6910b283936ae2ee098e02
WergSerg <wwwser181@outlook.com>
2021-10-06 22:12:32 +0300

    add readme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import config
 from lib.YandexAPI  import Weather, LocCorr
+from lib.CacheLib import ListDictBasedCache
 
 # -*- coding: utf-8 -*-
 
@@ -7,7 +8,21 @@ import telebot
 import json
 import requests
 
+
 bot = telebot.TeleBot(config.BOTAPI)
+
+cache=ListDictBasedCache()
+
+def cached(func):
+    def decor(args):
+        key=str(args.text).replace(' ','').lower()
+        if key in cache.keys():
+            func(args,cache[key])
+        else:
+            res=LocCorr().get_corr(args.text)
+            cache[key]=res
+            func(args,res)
+    return decor
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -22,18 +37,18 @@ def get_Location(message):
     res=aaa.getWeather(lat=lat,lon=lon)
     sendMess(message.from_user.id, makeMess(res=res), mode='Markdown')
 
+
 @bot.message_handler(content_types=['text'])
-def get_city(message):
-    resp=LocCorr().get_corr(message.text)
+@cached
+def get_city(message,resp):
     if resp[0]=='error':
-        # print(message.from_user.first_name)
         sendMess(message.from_user.id,'Ты что-то попутал!', mode='Markdown')
     else:
         aaa=Weather()
-        lon=resp[0]
-        lat=resp[1]
-        res=aaa.getWeather(lat=lat,lon=lon)
+        res=aaa.getWeather(lat=resp[1],lon=resp[0])
         sendMess(message.from_user.id, makeMess(res=res), mode='Markdown')
+
+
 
 def makeMess(res):
     mess = ''
